3f00a8a13e589a37dae86db0469219567cd45a71
Pamela Pelkola <pelkola.pam@gmail.com>
2018-06-26 21:56:51 -0500

    added on click event

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React,{ Component } from 'react';
 // import logo from './logo.svg';
-import Header from "./components/Jumbotron/Header";
+import Header from "./components/Header/Header";
 import Navbar from "./components/Nav/Navbar";
-import ScoreKeeper from './components/ScoreKeeper/ScoreKeeper';
-import TileCard from "./components/TileCard/TileCard";
-import tiles from "../Tiles.json";
-import Wrapper from "../Wrapper"
-// import ScoreKeeper from "./components/ScoreKeeper";
+import ScoreKeeper from './components/ScoreKeeper';
+import TileCard from "./components/TileCard";
+import Wrapper from "./components/Wrapper/Wrapper"
+import tiles from "./Tiles.json"
 import "./styles/App.css";
 
 class App extends Component {
@@ -14,7 +13,7 @@ class App extends Component {
     tiles
   };
 
-  removeTiles = id => {
+  moveTiles = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
     const tiles = this.state.tiles.filter(tile => tile.id !== id);
     // Set this.state.friends equal to the new friends array
@@ -25,10 +24,13 @@ class App extends Component {
   render() {
     return (
       <Wrapper>
-      
+      <Header />
+        <Navbar />
+        <ScoreKeeper />
+        
         {this.state.tiles.map(tile => (
           <TileCard
-            removeTiles={this.removeTiles}
+            moveTiles={this.moveTiles}
             id={tile.id}
             key={tile.id}
             image={tile.image}
@@ -41,13 +43,4 @@ class App extends Component {
 
 export default App;
 
-  // const App = () => (
-
-  // Setting this.state.friends to the friends json array
-  // <div>
-  //   <Header />
-  //   <Navbar />
-  //   <ScoreKeeper />
-  //   <TileCard />
-  // </div>
 
